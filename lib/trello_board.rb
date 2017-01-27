9571b3e2ee4e64ac30aa00fded28e5c2eb13cf6a
Osshrp <osshrp@gmail.com>
2017-01-27 16:27:32 +0300

    VC02 create LoadKeys class

--- lib/trello_board.rb
+++ lib/trello_board.rb
@@ -1,21 +1,23 @@
-require_relative 'trello_board/authorization'
+require_relative 'trello_board/load_keys'
 
 class TrelloBoard
-  def initialize(option = {})
-    @developer_public_key = kyes['developer_public_key']
-    @member_token         = kyes['member_token']
-    @boardid = option[:boardid]
-    client
+  def initialize(boardid:)
+    @keys = LoadKeys.keys
+    @board = RestClient.get("https://api.trello.com/1/boards/#{boardid}/lists",
+      {params: {key: @keys['developer_public_key'],
+                token: @keys['member_token']}})
   end
 
-  private
-
-  def kyes
-    YAML.load_file "#{ENV['HOME']}/.trellorc"
+  def lists
+    @board.body
   end
 
-  def client
-    @board = RestClient.get("https://api.trello.com/1/boards/#{@boardid}",
-      {params: {key: @developer_public_key, token: @member_token}})
+  def create_card!(title:, description:, list_id:)
+    RestClient.post('https://api.trello.com/1/cards',
+      { key: @keys['developer_public_key'],
+        token: @keys['member_token'],
+        idList: list_id,
+        name: title,
+        desc: description })
   end
 end
